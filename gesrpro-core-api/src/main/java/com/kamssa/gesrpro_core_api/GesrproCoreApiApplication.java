c252ddc64b1bce3379024d5dd4f59fa3af1ef92a
kamssa <21266863+kamssa@users.noreply.github.com>
2024-08-21 17:33:19 +0200

    ajout de core-api

---
+++ gesrpro-core-api/src/main/java/com/kamssa/gesrpro_core_api/GesrproCoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.kamssa.gesrpro_core_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesrproCoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesrproCoreApiApplication.class, args);
+	}
+
+}
---
+++ gesrpro-core-api/src/test/java/com/kamssa/gesrpro_core_api/GesrproCoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamssa.gesrpro_core_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesrproCoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
