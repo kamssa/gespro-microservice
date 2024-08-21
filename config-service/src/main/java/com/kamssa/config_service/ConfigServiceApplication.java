d5564d1800c7f6c0fe27c225b8dc4df3f5ab4d40
kamssa <21266863+kamssa@users.noreply.github.com>
2024-08-21 17:41:30 +0200

    ajout spring cloud

---
+++ config-service/src/main/java/com/kamssa/config_service/ConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kamssa.config_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceApplication.class, args);
+	}
+
+}
